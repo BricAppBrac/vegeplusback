07be5ce5aba7dd22e34e28cf286f7f2f075bd495
BricAppBrac <sandrinesteiger3000@gmail.com>
2023-09-13 07:24:15 +0200

    V0.3 signup avec envoi d'email

--- serverOLD.js
+++ serverOLD.js
@@ -10,26 +10,18 @@ const path = require("path");
 const { logger, logEvents } = require("./middleware/logger");
 const errorHandler = require("./middleware/errorHandler");
 const cookieParser = require("cookie-parser");
-
 const corsOptions = require("./config/corsOptions");
-
 const mongoose = require("mongoose"); // pourquoi ?
 const port = process.env.PORT || 5000;
 // const port = 5000;
+
 console.log(process.env.NODE_ENV);
 
-// Importation Nodemailer***
-const nodemailer = require("nodemailer");
+// connexion à la DB
+connectDB();
 
-// Configuration Nodemailer***
-const transporter = nodemailer.createTransport({
-  service: "yahoo",
-  auth: {
-    user: process.env.EMAIL_USERNAME,
-    pass: process.env.EMAIL_PASSWORD,
-  },
-});
-
+// on place le logger avant tout le reste
+app.use(logger);
 //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
 
 // Authorisation CORS
@@ -45,36 +37,6 @@ app.use(cors(corsOptions));
 // Middleware qui permet de traiter les données de la Request
 // Built-in Middleware
 app.use(express.json());
-
-// Route pour envoyer un e-mail***
-app.post("/send-email", async (req, res) => {
-  try {
-    const { to, subject, text } = req.body;
-
-    // Options de l'e-mail
-    const mailOptions = {
-      from: process.env.EMAIL_USERNAME,
-      to: to,
-      subject: subject,
-      text: text,
-    };
-
-    // Envoi de l'e-mail
-    await transporter.sendMail(mailOptions);
-    console.log("Email envoyé avec succès");
-    res.status(200).json({ message: "E-mail envoyé avec succès" });
-  } catch (error) {
-    console.log("Erreur lors de l'envoi de l'email: " + error);
-    console.error(error);
-    res.status(500).json({ message: "Erreur lors de l'envoi de l'e-mail" });
-  }
-});
-
-// connexion à la DB
-connectDB();
-
-// on place le logger avant tout le reste
-app.use(logger);
 
 //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
 //++++++++++  Ajout FullStack pour déploiement ++++++++++++//
